223e9c418eafaf79b3b9481914e4c8dc60df8448
Sandra Escandor-O'Keefe <sokeefe@fastly.com>
2026-03-21 12:22:34 -0230

    Add ALLOWED_HOSTS and specify 0.0.0.0

--- vulnerable_archive/vulnerable_archive/settings.py
+++ vulnerable_archive/vulnerable_archive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^3!*dt2xq2vs7+5%)j2&ii@h6tz5sq@mtkji--%2c!4ye2$lhm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
